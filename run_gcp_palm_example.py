d60415d2c0b8b1984e5b4b92f9ad52490900d492
rajib76 <rajib76@yahoo.com>
2023-06-11 17:08:36 -0700

    Added Palm API

--- run_gcp_palm_example.py
+++ run_gcp_palm_example.py
@@ -3,11 +3,6 @@ import os
 from dotenv import load_dotenv
 from langchain.llms import GooglePalm
 
-# Got below error
-# [links { description: "Google developers console API activation" url:
-# "https://console.developers.google.com/apis/api/generativelanguage.googleapis.com/overview?project=183578915041" }
-# , reason: "SERVICE_DISABLED" domain: "googleapis.com" metadata { key: "service" value:
-# "generativelanguage.googleapis.com" } metadata { key: "consumer" value: "projects/183578915041" } ]
 
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
